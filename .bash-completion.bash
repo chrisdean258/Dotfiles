293e277d0e4f30afcddbe5be4066aaaf75a31601
Chris Dean <chrisdean258@gmail.com>
2019-07-11 14:27:32 -0400

    cd completion .bash-completion.bash

--- .bash-completion.bash
+++ .bash-completion.bash
@@ -5,14 +5,12 @@ __complete_cd()
 	local cur dir
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
-	COMPREPLY=( $(compgen -d -G "${cur}*") )
+	COMPREPLY=( $(cat <(compgen -d) <(compgen -G "${cur}*") | sort | uniq -d) )
 
 	return 0
 }
 
-
-# complete -o default -F __complete_cd cd
-
+complete -o bashdefault -o default -o nospace -F __complete_cd cd
 
 __complete_vim()
 {
@@ -27,11 +25,11 @@ __complete_vim()
 
 	files=$(compgen -G "$cur*")
 
-	if [ "$files" ]; then
+	if [ -n "$files" ]; then
 		COMPREPLY=( $(echo "$files" | xargs file | grep -E "ASCII|text|empty" | cut -d: -f1 ) )
 	fi
 
 	return 0
 }
 
-# complete -o nosort -o default -F __complete_vim vim
+complete -o bashdefault -o default -o nospace -F __complete_vim vim
