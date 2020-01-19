d5751eb2995f57f4f16d1406ad40a5536b1a7e16
Chris Dean <chrisdean258@gmail.com>
2020-01-18 20:56:35 -0500

    .bash-completion.bash

--- .bash-completion.bash
+++ .bash-completion.bash
@@ -79,3 +79,18 @@ __complete_ssh()
 }
 complete -o bashdefault -o default -o nospace -F __complete_ssh ssh
 complete -o bashdefault -o default -o nospace -F __complete_ssh scp
+
+__complete_vim()
+{
+	local cur
+	local words
+
+	cur=${COMP_WORDS[COMP_CWORD]}	
+
+	words="$(grep -o "^.*:" | sed "s/:$//g")"
+	COMPREPLY=( $(compgen -W "$words" -- "$cur" ) )
+
+	return 0
+}
+
+complete -o bashdefault -o default -o nospace -F __complete_vim vim
